d032ee50e5c2435e9b16e011b4f41ee488e40edf
edutfree <168154164+edutfree@users.noreply.github.com>
2026-09-18 10:41:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,17 +10,9 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
-# sed -i 's/192.168.1.1/192.168.11.11/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.2/g' package/base-files/luci/bin/config_generate
+#sed -i 's/LEDE/OpenWrt(2026.9.18)/g' package/base-files/luci/bin/config_generate
+sed -i 's/LEDE/OpenWrt-'"$(date -d "now" +%Y-%m-%d)"'/g' package/base-files/luci/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/OpenWrt-2024.10.18/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# luci2 Modify default IP and hostname
-
-sed -i 's/192.168.1.1/192.168.11.2/g' package/base-files/luci2/bin/config_generate
-#sed -i 's/LEDE/OpenWrt(2025.3.21)/g' package/base-files/luci2/bin/config_generate
-sed -i 's/LEDE/OpenWrt-'"$(date -d "now" +%Y-%m-%d)"'/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.2/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/OpenWrt-'"$(date -d "now" +%Y-%m-%d)"'/g' package/base-files/files/bin/config_generate
